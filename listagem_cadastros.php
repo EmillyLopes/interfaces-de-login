679e759d27b555f3d3cf7b897b513513b365b63c
EmillyLopes <em.trabalho@outlook.com>
2021-11-24 23:23:32 -0300

    Versão 1.1  - Arrumando algumas interfaces de front

--- listagem_cadastros.php
+++ listagem_cadastros.php
@@ -14,8 +14,6 @@ mysqli_select_db($con, "bde" ) or die ("Banco de dados inexistente!");
 
 session_start();
 
-echo "<font face=\"Verdana\" size=2>Bem-Vindo " . $_SESSION["nome"] . "!<BR>\n";
-
 $query= sprintf("SELECT nome, idade, peso, altura  FROM cadastro");
 
 $dados= mysqli_query($con, $query) or die (mysqli_error());
@@ -29,19 +27,28 @@ $total = mysqli_num_rows($dados);
 	<link rel="stylesheet" type="text/css" href="layout.css" media="screen" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
     <title>Clientes cadastrados </title>
     </head>
 <body>
-    <?php
-        if($total > 0) {
-            do {
-				$nome=$linha["nome"];
-				$idade=$linha["idade"];
-				$peso=$linha["peso"];
-				$altura=$linha["altura"];
-?>           
-	<div class="tabela">
+<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-exclude" viewBox="0 0 16 16">
+  <path d="M0 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-2H2a2 2 0 0 1-2-2V2zm12 2H5a1 1 0 0 0-1 1v7h7a1 1 0 0 0 1-1V4z"/>
+</svg>
+<h1 class="text-center">Listagem de cadastros</h1>
+<div class="navegacao">
+	<ul class="nav nav-tabs nav justify-content-end">
+	<li class="nav-item">
+		<a class="nav-link" href="cadastro.html">Cadastrar</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link active" aria-current="page" href="#">Listar</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" href="sair.php">Sair</a>
+	</li>
+	</ul>
+</div>	
+<div class="tabela">
 		<table class="table table-striped">
 			<tr>
 				<th scope="col"><font color=red>Nome </font></td>
@@ -49,21 +56,24 @@ $total = mysqli_num_rows($dados);
 				<th scope="col"><font color=red>Peso</font></td>
 				<th scope="col"><font color=red>Altura</font><td>                    
 			</tr>
-			<?php
+    <?php
+        if($total > 0) {
+            do {
+				$nome=$linha["nome"];
+				$idade=$linha["idade"];
+				$peso=$linha["peso"];
+				$altura=$linha["altura"];
+
 				echo"<tr>";
 				echo"<td >$nome</td>";
 				echo"<td >$idade</td>";
 				echo"<td >$peso</td>";
 				echo"<td >$altura</td>";     	
 				echo"</div>";
-     
-			?> 
 
-	<?php
-		//}while($linha = mysqli_fetch_assoc($dados));
-		//}
+		}while($linha = mysqli_fetch_assoc($dados));
+		}
 	
             ?>
-		<button class="btn btn-outline-dark"><a href="sair.php">Sair</a></button>
 </body>
 </html>
